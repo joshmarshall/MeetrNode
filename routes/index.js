209e1bb448039ecabcdb75a0feaf79b0277487e2
Phred <fearphage@gmail.com>
2012-02-18 11:39:27 -0600

    pass all parameters to all includes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var fs = require('fs');
 
-module.exports = function(app){
+module.exports = function(app, express){
   fs.readdirSync(__dirname).forEach(function(file) {
-    if (file == 'index.js') return;
+    if (file.substr(-3) != '.js' || file == 'index.js') return;
     var name = file.substr(0, file.indexOf('.'));
-    require('./' + name)(app);
+    require('./' + name)(app, express);
   });
 
   app.get('/', function(req, res) {
