8cf9a7aa5e909d48bdf7ae2eb0e3d5de7ef744fd
Phred <fearphage@gmail.com>
2012-02-18 14:00:08 -0600

    updated tests to use base class

--- tests/test_people.py
+++ tests/test_people.py
@@ -1,10 +1,10 @@
-from unittest import TestCase
 import urllib2
 import json
+from base_testcase import BaseTestCase
 
-class TestServer(TestCase):
+class TestPeople(BaseTestCase):
 
-    def test_people(self):
+    def test_get_people(self):
         response = urllib2.urlopen('http://localhost:3000/people')
         self.assertEqual(response.getcode(), 200)
         self.assertEqual(response.info().getheader('Content-type'),
@@ -12,3 +12,8 @@ class TestServer(TestCase):
         data = json.loads(response.read())
         self.assertTrue('people' in data)
         self.assertTrue(type(data['people']), list)
+
+    def test_post_people(self):
+        response = self.post('http://localhost:3000/people',
+            {'uid': 1, 'name': 'Name'})
+        self.assertEqual(response.getcode(), 200)
