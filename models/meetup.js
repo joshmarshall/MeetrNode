34dba83adb6c63d718569dfd8142ebcd73d513c5
Josh Marshall <catchjosh@gmail.com>
2012-02-19 01:44:53 -0600

    Adding meetup 'get meetups for user' function.

--- models/meetup.js
+++ models/meetup.js
@@ -40,3 +40,26 @@ exports.get_meetup = function(client, meetup_id, callback) {
     callback(JSON.parse(result));
   });
 };
+
+exports.get_meetups_by_user_id = function(client, user_id, callback) {
+  var user_meetup_key = _get_user_meetup_key(user_id);
+  client.lrange(user_meetup_key, 0, -1, function(error, results) {
+    if (error) { throw error; }
+    var meetups = [];
+    var _get_next_meetup = function() {
+      var result_meetup_key = results.shift();
+      client.get(result_meetup_key, function(meetup_error, meetup_result) {
+        if (meetup_error) {
+          throw meetup_error;
+        }
+        meetups.push(JSON.parse(meetup_result));
+        if (results.length) {
+          _get_next_meetup();
+        } else {
+          callback(meetups);
+        }
+      });
+    };
+    _get_next_meetup(); // start the recursive querying
+  });
+};
--- spec/test_meetup_models.js
+++ spec/test_meetup_models.js
@@ -27,21 +27,32 @@ vows.describe("Models").addBatch({
         user_id: "tom"
       });
       var self = this;
+      var meetups = {};
       var client = utilities.connect(function() {
         meetup_models.save_meetup(client, meetup, function() {
           meetup_models.get_meetup(client, meetup.id, function(retrieved_meetup) {
-            self.callback(null, retrieved_meetup);
+            meetups.retrieved_meetup = retrieved_meetup;
+            meetup_models.get_meetups_by_user_id(client, "tom",
+              function(user_meetups) {
+                meetups.user_meetups = user_meetups;
+                self.callback(null, meetups);
+              }
+            );
           });
         });
       });
     },
 
-    "result in a saved meetup.": function(meetup) {
-      assert.equal(meetup.name, "Meetup");
+    "result in a saved meetup.": function(meetups) {
+      assert.equal(meetups.retrieved_meetup.name, "Meetup");
     },
 
-    "have the proper user id.": function(meetup) {
-      assert.equal(meetup.user_id, "tom");
+    "store the user id.": function(meetups) {
+      assert.equal(meetups.retrieved_meetup.user_id, "tom");
+    },
+
+    "be grouped by user.": function(meetups) {
+      assert.isTrue(meetups.user_meetups.length >= 1);
     }
   }
 }).export(module);
